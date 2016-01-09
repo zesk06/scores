05af75053069762aa297a6b591e0c0dad7b4e8a0
Zesk06 <zesk06@gmail.com>
2016-01-09 12:48:29 +0100

    Test the get_player_order method

--- scores_test.py
+++ scores_test.py
@@ -4,6 +4,7 @@
 "test scores.py"
 
 import scores
+from scores import Play, Player
 import pytest
 import os
 
@@ -37,6 +38,17 @@ def test_scores_dump():
     for play1, play2 in zip(mscores.plays, mscores2.plays):
         assert play1.date == play2.date
         assert play1.game == play2.game
+
+
+def test_play():
+    "test the play class"
+    myplay = Play()
+    myplay.players.append(Player('un', 100))
+    myplay.players.append(Player('deux', 10))
+    myplay.players.append(Player('trois', 1))
+    order = myplay.get_player_order()
+    assert len(order) == 3
+    assert order == [(100, ['un']), (10, ['deux']), (1, ['trois'])]
 
 if __name__ == '__main__':
     pytest.main()
