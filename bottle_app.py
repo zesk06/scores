8bc3faf7a02442290048eb03085160d1ae7286fe
Zesk06 <zesk06@gmail.com>
2016-01-13 07:15:08 +0000

    Bottlefied scores !

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,17 @@
 
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route
+import scores
+import os
+
+THIS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 @route('/')
 def hello_world():
-    return 'Hello from Bottle!'
+    MSCORES = scores.Scores(filename=os.path.join(THIS_DIR, 'scores.json'))
+    STATS = scores.OverallWinnerStat()
+    STATS.parse(MSCORES)
+    return STATS.get_html()
 
 application = default_app()
 
