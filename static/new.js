80a2447c433b0eee9036d8b343b37329662c5617
Zesk06 <zesk06@gmail.com>
2016-02-26 06:43:34 +0100

    Add a date button to set today's date

--- static/new.js
+++ static/new.js
@@ -1,7 +1,22 @@
 var main = function(){
-    $('.player-stat .panel-body').hide();
-    $('.player-stat-btn').click(function(){
-        $(this).parents('.player-stat').find('.panel-body').toggle()
+    $('.date-btn').click(function (){
+        // Dates in javascript is fun, to tostring method?
+        var today = new Date();
+        var curr_date =  today.getDate();
+        if (curr_date < 10){
+            curr_date = '0' + curr_date;
+        }
+
+        var curr_month = today.getMonth() + 1; //Months are zero based
+        if (curr_month < 10){
+            curr_month = '0' + curr_month;
+        }
+
+        var curr_year =  today.getFullYear();
+        if (curr_year > 2000){
+            curr_year = curr_year - 2000;
+        }
+       $('#dateId').val(curr_date+'/'+curr_month+'/'+curr_year);
     });
 }
 
